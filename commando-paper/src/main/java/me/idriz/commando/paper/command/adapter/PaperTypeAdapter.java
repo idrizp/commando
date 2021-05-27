916ec744a2eea99c33490eae1e6e0857f6a5f68e
idrizp <idrizpelaj@hotmail.com>
2021-05-28 01:29:02 +0200

    Renamed commando-examples module, added paper type adapter abstraction

---
+++ commando-paper/src/main/java/me/idriz/commando/paper/command/adapter/PaperTypeAdapter.java
@@ -0,0 +1,8 @@
+package me.idriz.commando.paper.command.adapter;
+
+import me.idriz.commando.adapter.TypeAdapter;
+import me.idriz.commando.paper.sender.PaperSender;
+
+public interface PaperTypeAdapter<T> extends TypeAdapter<T, PaperSender> {
+
+}
